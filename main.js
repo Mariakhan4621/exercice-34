60600cd58b4104f1b04337854be332847ab31aea
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-15 18:15:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var favourite_pizza = ["pepperool", "chikrn", "veg"];
+//for (let pizza of favourite_pizza) {
+// console.log(pizza)
+//}
+//console.log ("\n")
+for (var _i = 0, favourite_pizza_1 = favourite_pizza; _i < favourite_pizza_1.length; _i++) {
+    var pizza = favourite_pizza_1[_i];
+    console.log(" I really like $(pizza) pizza");
+}
+console.log("\nI really love pizzal");
---
+++ main.ts
@@ -0,0 +1,14 @@
+let favourite_pizza : string [] = ["pepperool" , "chikrn" , "veg"]
+
+//for (let pizza of favourite_pizza) {
+   // console.log(pizza)
+//}
+
+//console.log ("\n")
+
+for (let pizza of favourite_pizza){
+    console.log(" I really like $(pizza) pizza")
+}
+
+console.log ("\nI really love pizzal")
+
